ea8297906f99d9a01d406d47870f2846fcaafbec
Josh Hamilton <nearbycoder@gmail.com>
2017-12-21 12:53:42 -0600

    update auth to mutations

--- app/graphql/mutations/auth_mutation.rb
+++ app/graphql/mutations/auth_mutation.rb
@@ -2,10 +2,43 @@ module Mutations::AuthMutation
   def self.create(object_type)
     object_type.field :register_user, Types::AuthType do
       description 'Update a current user'
-      argument :user, UserCreateType
+      argument :user, !UserCreateType
       resolve ->(_root, args, ctx) {
         ModelServices::AuthService.new(args, ctx).create
       }
+    end
+
+    object_type.field :sign_in_user do
+      type Types::AuthType
+      argument :user, !UserSignInType
+      description 'Sign in User'
+      resolve ->(_obj, args, _ctx) do
+        user = User.find_by(email: args[:user][:email])
+        if user && user.valid_password?(args[:user][:password])
+          auth = user.create_new_auth_token
+          auth['user'] = user
+          auth
+        else
+          RecursiveOpenStruct.new(errors: { full_messages: ['Invalid Credentials'] })
+        end
+      end
+    end
+
+    object_type.field :sign_out_user do
+      type Types::SignOutType
+      argument :client, !types.String
+      argument :uid, !types.String
+      description 'Sign out User'
+      resolve ->(_obj, args, _ctx) do
+        user = User.find_by(email: args[:uid])
+        if user && args[:client] && user.tokens[args[:client]]
+          user.tokens.delete(args[:client])
+          user.save!
+          true
+        else
+          false
+        end
+      end
     end
 
     object_type.field :update_current_user, Types::UserType do
@@ -22,6 +55,19 @@ module Mutations::AuthMutation
         ModelServices::AuthService.new(args, ctx).destroy
       }
     end
+  end
+end
+
+UserSignInType = GraphQL::InputObjectType.define do
+  name 'UserSignInType'
+  description 'Properties for signing in a user'
+
+  argument :email, !types.String do
+    description 'Email of the user.'
+  end
+
+  argument :password, !types.String do
+    description 'Password of the user.'
   end
 end
 
--- app/graphql/queries/auth_query.rb
+++ app/graphql/queries/auth_query.rb
@@ -1,40 +1,5 @@
 module Queries::AuthQuery
   def self.create(object_type)
-    object_type.field :sign_in_user do
-      type Types::AuthType
-      argument :email, !types.String
-      argument :password, !types.String
-      description 'Sign in User'
-      resolve ->(_obj, args, _ctx) do
-        user = User.find_by(email: args[:email])
-        if user && user.valid_password?(args[:password])
-          auth = user.create_new_auth_token
-          auth['user'] = user
-          auth
-        else
-          raise Pundit::NotAuthorizedError
-        end
-      end
-    end
-
-    object_type.field :sign_out_user do
-      type Types::SignOutType
-      argument :token, !types.String
-      argument :client, !types.String
-      argument :uid, !types.String
-      description 'Sign out User'
-      resolve ->(_obj, args, _ctx) do
-        user = User.find_by(email: args[:uid])
-        if user && args[:client] && user.tokens[args[:client]]
-          user.tokens.delete(args[:client])
-          user.save!
-          true
-        else
-          false
-        end
-      end
-    end
-
     object_type.field :valid_token do
       type Types::UserType
       description 'Validate User'
